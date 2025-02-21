3fb9059ce7a35b1b22f4a9a411843d110d3b204b
robim16 <luis.hoyos@dydsoluciones.net>
2025-02-21 17:37:15 -0500

    creación del modelo user

--- components/layout/TopBar.jsx
+++ components/layout/TopBar.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+"use client"
+import { SignedIn, SignOutButton } from '@clerk/clerk-react'
+import { Add, Logout, Search } from '@mui/icons-material'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React, { useState } from 'react'
 
 const TopBar = () => {
+  const router = useRouter()
+  const [search, setSearch] = useState('')
+
   return (
     <div className='flex justify-between items-center mt-6'>
       <div className='relative'>
-        <input type="text" className='search-bar'/>
+        <input type="text" className='search-bar' placeholder='Search posts, people, ...' value={search}
+          onChange={(e) => setSearch(e.target.value)} />
+        <Search className='search-icon' onClick={() => { }} />
       </div>
-      <button>Create A Post</button>
+      <button className='create-post-btn' onClick={() => router.push("/create-post")}>
+        <Add />Create A Post</button>
+
+      <div className="flex gap-3">
+        <SignedIn>
+          <SignOutButton>
+            <div className='flex cursor-pointer items-center md:hidden'>
+              <Logout sx={{ color: "white", fontSize: "32px" }} />
+            </div>
+          </SignOutButton>
+        </SignedIn>
+        <Link href="/">
+        <Image src="/assets/phucmai.png" alt='profile photo' width={50} height={50}
+          className='rounded-full md:hidden'></Image>
+      </Link>
+      </div>
     </div>
   )
 }
