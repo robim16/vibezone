4ec78766b2a54819e2364d5369927aeb9ff2cc64
robim16 <luis.hoyos@dydsoluciones.net>
2025-03-22 17:29:54 -0500

    search users function

---
+++ app/api/user/search/[query]/route.js
@@ -0,0 +1,23 @@
+import { connectToDB } from "../../../../../lib/mongodb/mongoose"
+import User from "../../../../../lib/models/User"
+
+export const GET = async (req, { params }) => {
+    const { query } = params
+
+    try {
+        await connectToDB()
+
+        const searchedUsers = await User.find({
+            $or: [
+                { username: { $regex: query, $options: "i" }},
+                { name: { $regex: query, $options: "i"}}
+            ]
+
+        }).exec()
+
+        return new Response(JSON.stringify(searchedUsers), { status: 200})
+    } catch (error) {
+        console.log(error);
+        return new Response("Failed to get users by search", { status: 500 })
+    }
+}
